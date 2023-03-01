9a2c8bc5b7bcd82b775c403a6bf9533d70b1373b
Keadeish <you@example.com>
2023-03-01 15:44:11 +0000

    started the about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import { Header } from "./components/Header";
 function App() {
   return (
-    <div>hi</div>
+    <div>
+      <Header />
+      <p className="title">KEADEISH MORRISON</p>
+      <p className="subheading">
+        Full-Stack Software Engineer [Under Development].
+      </p>
+    </div>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export function Header(): JSX.Element {
+  return (
+    <>
+      <div id="headerContainer">
+        <div id="about">ABOUT</div>
+        <div id="projects">PROJECTS</div>
+        <div id="experience">EXPERIENCE</div>
+        <div id="journey">JOURNEY</div>
+      </div>
+    </>
+  );
+}
