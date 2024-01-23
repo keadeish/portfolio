966403f12438919c5dfef10c0f90f7d5b66bf494
Keadeish <you@example.com>
2024-01-23 00:27:46 +0000

    added cv page onto experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,8 +7,9 @@ export function Experience(): JSX.Element {
       <Header selectedLink={name} />
       <p className="title">Experience</p>
       <p className="subheading">
-        <i>Full-Stack Software Engineer [Under Development].</i>
+        <i>Greetings. I'm London-based <b>full-stack software engineer</b> that has graduated from <a href="https://academy.tech/"><b>Academy's software development program.</b></a> <br/>I have experience with both <b>JavaScript & TypeScript</b> and I'm an avid user of libraries commonly used for testing & automation, such as <b>Puppeteer.js</b>.</i>
       </p>
+      <div id="cvBox">CV</div>
     </div>
   );
 }
