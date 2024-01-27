5dc7902a62afc72a8ee3cf04d8706ebe936161ef
Keadeish <you@example.com>
2024-01-27 23:49:12 +0000

    added dynamic underlining of page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,29 @@
 import { Link } from "react-router-dom";
 import { ILinks } from "../Utils/Interfaces";
 export function Header({ selectedLink }: ILinks): JSX.Element {
-  //fix hyperlink colour and underline
   let pages = ["about", "projects", "experience", "journey"];
   return (
     <>
       <div id="headerContainer">
         {pages.map((page, i) => {
           if (page === "about") {
+            if (selectedLink === "about") {
+              return (
+                <Link id={`${selectedLink}Selected`} to={`/`} key={i}>
+                  {page.toLocaleUpperCase()}
+                </Link>
+              );
+            } else
+              return (
+                <Link id={`${page}`} to={`/`} key={i}>
+                  {page.toLocaleUpperCase()}
+                </Link>
+              );
+          }
+
+          if (selectedLink === page) {
             return (
-              <Link id={`${page}`} to={`/`} key={i}>
+              <Link id={`${selectedLink}Selected`} to={`/${page}`} key={i}>
                 {page.toLocaleUpperCase()}
               </Link>
             );
--- src/components/Years.tsx
+++ src/components/Years.tsx
@@ -1,9 +1,9 @@
 import { IJourney } from "../Utils/Interfaces";
 export function Years(props: IJourney): JSX.Element {
-const isFirstYear = props.year === 2024
+  const isFirstYear = props.year === 2024;
   return (
     <div id="yearCard">
-      <div id='year' style={{marginTop: isFirstYear ? '0vh' : '25vh'}}>
+      <div id="year" style={{ marginTop: isFirstYear ? "0vh" : "25vh" }}>
         <p>
           <b>{props.year}</b>
         </p>
